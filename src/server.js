5c9bacd63104e1d71bc55625f1a43e07ac4525bf
vapvin <vins@vins.dev>
2020-07-01 20:27:07 +0900

    Add Morgan Logger : starting server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+require("dotenv").config()
+import {GraphQLServer} from 'graphql-yoga';
+import logger from 'morgan';
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => "HELLO"
+    }
+}
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+server.express.use(logger("dev"));
+
+server.start({port: PORT}, () => console.log(`Server on port http://127.0.0.1:${PORT}`));
